a207979a162e760cad1c5c7728435606614af9f4
Tail-R <tr.wx58@gmail.com>
2025-08-03 19:29:26 +0900

    Created install_docker.yaml and install_k8s.yaml

--- play.sh
+++ play.sh
@@ -1,6 +1,12 @@
 #! /usr/bin/env bash
 
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
+set -euo pipefail
+
+SSH_KEY="${HOME}/.ssh/id_ed25519"
+
+if [ -z "${SSH_AUTH_SOCK-}" ] || ! ssh-add -l &> /dev/null; then
+    eval "$(ssh-agent -s)"
+    ssh-add "${SSH_KEY}"
+fi
 
 ansible-playbook site.yaml --ask-become-pass
